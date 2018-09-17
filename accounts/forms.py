cfd4b6f41bb947ee5adb1085ad87c13d5a45b846
Gabriela Guedez Hamerlok <gaby.guedez@gmail.com>
2018-09-17 19:23:31 +0000

    created static and media folder in S3

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 
 class UserLoginForm(forms.Form):
     """ Form to be used to login users"""
-    username = forms.CharField()
+    username_or_email = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
 class UserRegistrationForm(UserCreationForm):
